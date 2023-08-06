6c2879f3a36628ca980598e6224f5069c5f71403
yasminlopes <yclopes27@gmail.com>
2023-08-06 19:58:39 -0300

    feat: create form

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,20 @@ import React from 'react';
 
 function App() {
   return (
-    <h1 className="text-3xl font-bold underline">
-    Hello world!
-  </h1>
+   <div className="flex w-full h-screen items-center justify-center">
+
+     <form>
+      <input
+      type="text"
+      className="p-2 rounded-lg outline-none"
+      placeholder="Busque a cidade..."/>
+      <button
+      className="bg-blue-600 p-3 rounded-lg ml-3 text-white font-bold">
+        Pesquisar
+        </button>
+    </form>
+
+   </div>
   )
 }
 
