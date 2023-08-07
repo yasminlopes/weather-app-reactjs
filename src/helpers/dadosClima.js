f4060d35532684e96c95510054294adc47b55687
yasminlopes <yclopes27@gmail.com>
2023-08-06 22:17:37 -0300

    feat: add helper method to get data

---
+++ src/helpers/dadosClima.js
@@ -0,0 +1,16 @@
+const dadosClima = {
+  location: {
+    name: "-",
+    region: "-",
+    country: "-",
+  },
+  current: {
+    temp_c: 0,
+    condition: {
+      text: '-',
+      icon: "//cdn.weatherapi.com/weather/64x64/night/113.png",
+    },
+  },
+};
+
+export default dadosClima;
