2f804d5f91872bbc473994c2478e38b4c000ecb7
yasminlopes <yclopes27@gmail.com>
2023-08-06 22:19:10 -0300

    feat: add methods to search the city and fetch data

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,35 @@
-import React from 'react';
+import React, { useState } from "react";
+import buscarCidade from "./services/weatherApi";
+import dadosClima from "./helpers/dadosClima";
+import ClimaCard from "./components/ClimaCard/ClimaCard";
 
 function App() {
+  const [nomeCidade, setNomeCidade] = useState("");
+  const [cidade, setDadosCidade] = useState(dadosClima);
+
+  const pesquisarCidade = async (event) => {
+    event.preventDefault();
+
+    await buscarCidade(nomeCidade).then((resposta) => setDadosCidade(resposta));
+  };
   return (
-   <div className="flex w-full h-screen items-center justify-center">
-
-     <form>
-      <input
-      type="text"
-      className="p-2 rounded-lg outline-none"
-      placeholder="Busque a cidade..."/>
-      <button
-      className="bg-blue-600 p-3 rounded-lg ml-3 text-white font-bold">
-        Pesquisar
+    
+    <div className="flex flex-col w-full h-screen items-center justify-center min-w-[220px]">
+      <form onSubmit={pesquisarCidade} className="flex items-center">
+        <input
+          type="text"
+          className="p-2 rounded-lg outline-none border border-gray-300"
+          value={nomeCidade}
+          onChange={({ target: { value } }) => setNomeCidade(value)}
+          placeholder="Busque a cidade..."
+        />
+        <button className="bg-blue-600 p-2 rounded-lg ml-3 text-white font-bold" type="submit">
+          Pesquisar
         </button>
-    </form>
-
-   </div>
-  )
+      </form>
+      <ClimaCard cidade={cidade} />
+    </div>
+  );
 }
 
 export default App;
